a5b7d9c3da44e4e04ba28a5a7eb314af7f5573a6
Ermias80 <ermiasmaz8069@gmail.com>
2024-08-14 13:31:02 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 
-import requests  # Alphabetical order for imports
+import requests
+
 
 def number_of_subscribers(subreddit):
-  """
-  Queries the Reddit API for the subscriber count of a subreddit.
+    """
+    Queries the Reddit API for the subscriber count of a subreddit.
 
-  Args:
+    Args:
       subreddit: The name of the subreddit to query.
 
-  Returns:
+    Returns:
       The number of subscribers (integer) or 0 if the subreddit is invalid.
-  """
-  url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-  headers = {
-      "User-Agent": "script:0x16-api_advanced:0.1 (by u/ Careful_pin_)"
-  }
-  response = requests.get(url, headers=headers, allow_redirects=False)
-  if response.status_code == 404:
-      return 0
-  results = response.json().get("data")
-  return results.get("subscribers")
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "script:0x16-api_advanced:0.1 (by u/Careful_pin_)"  # Removed space before 'Careful_pin_'
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
+
